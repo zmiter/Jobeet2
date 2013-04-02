cb73b6087de4c9d29806ea2bfd4b2c88899cee83
Jeff Mott <Jeff.Mott.OR@gmail.com>
2013-04-02 14:17:47 -0700

    DOCTRINE EXTENSIONS CONT.
    A Symfony application won't load a bundle just because it exists on the filesystem. Our app will load only those bundles that we tell it to load, so we need to tell it to load the DoctrineExtensionsBundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Jobeet\JobBoardBundle\JobeetJobBoardBundle(),
         );
 
